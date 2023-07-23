abff027821471c0a6903d109e01fdd6ff2f391cf
Battula Rohith <136285378+BattulaRohith@users.noreply.github.com>
2023-07-23 20:11:13 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+ let movies =[
+    {
+        name:'Jhansi',
+        des:'The show revolves around Jhansi, a boutique owner whos staying with her partner and his daughter. .',
+        image:'images/slider-1.png'
+    },
+    {
+        name:'The Warrior',
+        des:'A young, naïve and idealistic Satya moves to Kurnool in hopes of a bright future. Discontent with how the city remains in the grip of a gangster, he decides to change the ‘treatment’ for the ‘disease’.',
+        image:'images/slider-2.png'
+    },
+    {
+        name:'The Exposed',
+        des:'Akash (Chennamaneni Vasudev Rao) and Greeshma (Harshitha) are a married couple who are noted journalists working for a TV channel named Exposed. Greeshma',
+        image:'images/slider-3.png'
+    },
+    {
+        name:'Vikram Vedha',
+        des:'Akash (Chennamaneni Vasudev Rao) and Greeshma (Harshitha) are a married couple who are noted journalists working for a TV channel named Exposed. Greeshma',
+        image:'images/slider-4.png'
+    },
+    {
+        name:'Bheemla Nayak',
+        des:'Akash (Chennamaneni Vasudev Rao) and Greeshma (Harshitha) are a married couple who are noted journalists working for a TV channel named Exposed. Greeshma',
+        image:'images/slider-5.png'
+    }
+ ]
